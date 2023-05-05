0b6171437e272d2f88a0c683f647a51d0bfd8f22
Eugene Gorokhovich <121453815+IEugeneGoI@users.noreply.github.com>
2023-05-05 16:44:48 +0300

    Add files via upload

---
+++ ishop2/Ishop.js
@@ -0,0 +1,67 @@
+﻿var Ishop = React.createClass({
+
+  displayName: 'Ishop',
+
+  propTypes: {
+    header: React.PropTypes.string.isRequired,
+    content:React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        code: React.PropTypes.number.isRequired,
+        name: React.PropTypes.string.isRequired,
+        url: React.PropTypes.string.isRequired,
+        price: React.PropTypes.number.isRequired,
+        inStock: React.PropTypes.number.isRequired,
+      })
+    )
+  },
+
+  getInitialState: function() {
+    return {
+      selectedProductCode: null,
+      items: this.props.content
+    };
+  },
+
+  productSelected: function (code) {
+    this.setState({selectedProductCode: code})
+  },
+
+  deleteItem: function () {
+    let answer = confirm('Delete?');
+    if (answer) {
+      var filterArr = this.state.items.filter(item =>
+        item.code !== this.state.selectedProductCode
+      );
+      this.setState({items: filterArr});
+    }
+  },
+
+  setDeleteCode: function (code) {
+    this.setState({selectedProductCode: code}, this.deleteItem)
+  },
+
+  render: function () {
+    var productItem = this.state.items.map(v =>
+        React.createElement(Product, {key: v.code, code: v.code,
+          name: v.name, price: v.price, url: v.url, inStock: v.inStock,
+          cbSelected: this.productSelected, cbDeleted: this.setDeleteCode,
+          isSelected: (v.code == this.state.selectedProductCode)})
+    );
+
+    return React.DOM.div({className: 'Ishop'},
+            React.DOM.header(null, this.props.header),
+            React.DOM.table(null, React.DOM.thead(null,
+                React.DOM.tr(null,
+                    React.DOM.th({className: 'cell'}, 'Name'),
+                    React.DOM.th({className: 'cell'}, 'Price'),
+                    React.DOM.th({className: 'cell'}, 'URL'),
+                    React.DOM.th({className: 'cell'}, 'Quantity'),
+                    React.DOM.th({className: 'cell'}, 'Control')
+                )
+              ),
+              React.DOM.tbody({className: 'body'}, productItem)
+            ),
+
+    );
+  }
+});
---
+++ ishop2/Product.js
@@ -0,0 +1,35 @@
+﻿var Product = React.createClass({
+  displayName: 'Product',
+
+  propTypes: {
+    code: React.PropTypes.number.isRequired,
+    name: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired,
+    price: React.PropTypes.number.isRequired,
+    inStock: React.PropTypes.number.isRequired,
+    cbSelected: React.PropTypes.func.isRequired,
+    cbDeleted:  React.PropTypes.func.isRequired,
+    isSelected: React.PropTypes.bool.isRequired
+  },
+
+  selectProduct: function () {
+    this.props.cbSelected(this.props.code);
+  },
+
+  deleteIProduct: function() {
+    this.props.cbDeleted(this.props.code)
+  },
+
+  render: function() {
+    return React.DOM.tr({key: this.props.code, onClick: this.selectProduct, style: {background: this.props.isSelected ? 'orange' : 'white'}},
+            React.DOM.td({className: 'cell'}, this.props.name),
+            React.DOM.td({className: 'cell'}, this.props.price),
+            React.DOM.td({className: 'cell'}, this.props.url),
+            React.DOM.td({className: 'cell'}, this.props.inStock),
+            React.DOM.td({className: 'cell'},
+              React.DOM.input({type:'button',value:'Delete', onClick: this.deleteIProduct})
+            ),
+          );
+  }
+});
+
