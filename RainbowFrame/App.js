d24ad09a529be52df6968731daa4db838c4b9d35
Eugene Gorokhovich <121453815+IEugeneGoI@users.noreply.github.com>
2023-05-17 15:33:12 +0300

    Homework 4

---
+++ RainbowFrame/App.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import FrameRender from './components/FrameRender';
+
+ReactDOM.render(
+  <FrameRender/>, 
+  document.getElementById('container') 
+);
+
