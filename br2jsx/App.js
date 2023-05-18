bf417179434b2fc509de31ac04ef62ae4984f492
Eugene Gorokhovich <121453815+IEugeneGoI@users.noreply.github.com>
2023-05-18 15:47:41 +0300

    Add files via upload

---
+++ br2jsx/App.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TextRender from './components/TextRender';
+
+ReactDOM.render(
+  <TextRender/>, 
+  document.getElementById('container') 
+);
+
