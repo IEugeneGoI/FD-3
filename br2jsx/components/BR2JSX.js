b645cb8c804c3bb23a05d2c01edcf7f8b7c3a698
Eugene Gorokhovich <121453815+IEugeneGoI@users.noreply.github.com>
2023-05-18 15:49:46 +0300

    Add files via upload

---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,35 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './BR2JSX.css';
+
+class BR2JSX extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+  
+  render () {
+    const regexp = /(?:<br>|<br\/>|<br\s+\/>)+/gi;
+    const text = this.props.text.split(regexp);
+
+    const textArr = [];
+
+    for(let i = 0; i < text.length; i++) {
+      if(i == text.length - 1) {
+        textArr.push(text[i]);
+        console.log(text[i])
+      } else {
+        textArr.push(text[i], <br key={i}/>);
+      }
+
+    }
+
+    return (
+      <div className='br2jsx'>
+        {textArr}
+      </div>
+    );
+  }
+}
+
+export default BR2JSX;
