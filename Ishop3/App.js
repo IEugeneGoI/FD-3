9bc557473e1f2fa26a29c7305cfb3d714716234f
Eugene Gorokhovich <121453815+IEugeneGoI@users.noreply.github.com>
2023-06-01 12:17:28 +0300

    Homework 7

---
+++ Ishop3/App.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Ishop from './components/Ishop';
+import itemsArr from './items.json';
+
+var shopName = 'Магазин компьютерной техники';
+
+ReactDOM.render(
+  <Ishop name = {shopName}  items = {itemsArr}/>, 
+  document.getElementById('container') 
+);
+
